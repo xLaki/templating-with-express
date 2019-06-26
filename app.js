186496c3ed04600c47f77ea35df7e330e54e4d34
xLaki <brandonbedard12@gmail.com>
2019-06-26 10:12:56 -0400

    added an ejs file and linked it with the other files

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 
+// adding middleware
+// setting up template engine
+app.set("view engine", "ejs");
+
 app.get('/', function(req, res){
     console.log('bruh moment');
-    res.send('bruhh');
+    res.render('home');
 });
 
 app.listen(3000, function(err){
     if (err){
         console.log(err);
     };
+    console.log('Server is live, and so is your fat ugly body.')
 });
